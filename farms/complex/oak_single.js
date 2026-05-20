190731db9b29db7fd499ea638124b93fb00abf74
borito185 <88558971+Borito185@users.noreply.github.com>
2026-05-21 01:02:20 +0200

    sneaky bastard

---
+++ farms/complex/oak_single.js
@@ -0,0 +1,39 @@
+bot = require("../../bot.js")
+module.exports = function () {
+    let row_elements = 13
+    let col_elements = 21
+    let offset = 5
+    let drops = [bot.item.of("oak_log"), bot.item.of("apple")]
+
+    bot.start()
+    bot.progress.init(row_elements * col_elements)
+
+    do_tree = dir => bot.action.complex.do_tree(dir, offset, {
+        do_grow: false,
+        grow_time: 0,
+        do_mine: true,
+        mine_time: 1650,
+        do_plant: true,
+        increment_progress: true
+    })
+
+    function do_row(start_dir, direction) {
+        do_tree(start_dir)
+        for (let i = 0; i < row_elements-1; i++) {
+            do_tree(direction)
+        }
+
+        bot.look.towards(direction+140,55)
+        drops.forEach(bot.item.drop_all_of)
+    }
+    for (let i = 0; i < col_elements; i++) {
+        do_row(
+            i === 0 ? bot.dir.EAST : bot.dir.NORTH,
+            i%2 === 0 ? bot.dir.EAST : bot.dir.WEST,
+        )
+    }
+    bot.action.move_mine(bot.dir.SOUTH, offset * (col_elements-1), true, 0)
+    bot.action.move_mine(bot.dir.WEST, offset * row_elements, true, 0)
+
+    bot.finish()
+}
