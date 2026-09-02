459d45a2bb51466566b8d586a01a9092bb82dfff
borito185 <88558971+Borito185@users.noreply.github.com>
2026-09-02 11:09:29 +0200

    fix: simple fix to prevent service closing while running

--- commands/load_commands.js
+++ commands/load_commands.js
@@ -1,3 +1,4 @@
-﻿const manager = JsMacros.getServiceManager();
+﻿Time.sleep(100) // wait a bit so service closes
+const manager = JsMacros.getServiceManager();
 manager.unregisterService("Azora Bot")
 manager.registerService("Azora Bot", __dirname + "/command_service.js", true)
